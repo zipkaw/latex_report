5ad0482c0ae26e7a4fde2904e6db1030784d4c33
Edward Senkevich <edward.senkevich@axonim.by>
2023-09-22 00:53:12 +0300

    add sys, schemas, delete bib

--- compile.ps1
+++
@@ -1,2 +0,0 @@
-docker run -i --rm -v "$(Get-Location):/test" -w /test ajiob/docker-xelatex-fonts:1.2.1 make -j4 -C "main_report" all
-docker run -i --rm -v "$(Get-Location):/test" -w /test ajiob/docker-xelatex-fonts:1.2.1 make -j4 -C "practice_report" all
